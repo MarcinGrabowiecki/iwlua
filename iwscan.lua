150bec944e057fff8e0d4ba337bd1cca44a2b765
marcin.grabowiecki@gmail.com <marcin.grabowiecki@gmail.com>
2016-01-05 22:09:12 +0100

    added adress

--- iwscan.lua
+++ iwscan.lua
@@ -20,13 +20,13 @@ for l in f:lines () do
 		r[#r+1] = tt
 		tt={}
 		add("cellnum",cellnum)
+		add("address",address)
 	end
 	add("essid",string.match(l,"ESSID:.(.*)."))
-	add("channel",string.match(l,"Channel:(.*)"))
+	add("channel",string.match(l,"Channel:(%d+)"))
 	add("quality",string.match(l,"Quality=(%d+).*"))
 
 	--	cellNum,n,cellAddr=string.match(l,"(%d+)(............)(.................)")
-	--	n,channel=string.match(l,"(Channel:)(%d+)")
 	end
 r[#r+1] = tt
 table.remove(r,1)
@@ -34,10 +34,10 @@ table.remove(r,1)
 function bar(q)
 	local ret=""
 	for i=0,q,1 do
-		ret=ret.."#"
+		ret=ret.."|"
 	end
 	for i=0,70-q,1 do
-		ret=ret.."_"
+		ret=ret.."-"
 	end
 	return ret
 end
@@ -51,7 +51,7 @@ table.sort(r,function(a,b) return a.quality>b.quality end)
 for i,c in pairs(r) do
 	if c.quality==nil then else
 		hist[#hist+1] = c
-		print(col(i,3)..col(c.cellnum,3)..col(c.channel,3)..col(c.essid,18)..col(c.quality,3)..bar(tonumber(c.quality)))
+		print(col(i,3)..col(c.cellnum,3)..col(c.channel,3)..col(c.essid,18)..col(c.address,18)..col(c.quality,3)..bar(tonumber(c.quality)))
 	end
 end
 

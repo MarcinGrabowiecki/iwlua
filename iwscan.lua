faef162900e31941021c951e9103343c5d3a170f
marcin.grabowiecki@gmail.com <marcin.grabowiecki@gmail.com>
2016-01-05 22:44:45 +0100

    moved processing to function, net scan loop

--- iwscan.lua
+++ iwscan.lua
@@ -1,61 +1,72 @@
 #!/usr/bin/lua
-local f = assert (io.popen ("/sbin/iwlist scan"))
-local r={}
-local tt={}
-local hist={}
 
-function add(k,v)
+local ansiPrefix = string.char(27).."["
+local goto00 = ansiPrefix.."0;0H"
+
+
+function add(k,v,tt)
 	if v==nil then return end
 	tt[k]=v
 end	
 
-function pc(s)
-	print(string.char(27).."["..s)
+
+
+function proces()
+
+	local f = assert (io.popen ("/sbin/iwlist scan"))
+	local r={}
+	local tt={}
+	local hist={}
+
+	for l in f:lines () do
+		--print (l)
+		cellnum,address = string.match(l,"Cell (%d+)............(.*)")
+		if cellnum then
+			r[#r+1] = tt
+			tt={}
+			add("cellnum",cellnum,tt)
+			add("address",address,tt)
+		end
+		add("essid",string.match(l,"ESSID:.(.*)."),tt)
+		add("channel",string.match(l,"Channel:(%d+)"),tt)
+		add("quality",string.match(l,"Quality=(%d+).*"),tt)
+		--	cellNum,n,cellAddr=string.match(l,"(%d+)(............)(.................)")
+		end
+	r[#r+1] = tt
+	table.remove(r,1)
+
+	function bar(q)
+		local ret=""
+		for i=0,q,1 do
+			ret=ret.."|"
+		end
+		for i=0,70-q,1 do
+			ret=ret.."-"
+		end
+		return ret
+	end
+
+	function col(s,n)
+		return string.sub(s.."                              ",0,n)
+	end
+
+	table.sort(r,function(a,b) return a.quality>b.quality end)
+
+	print(goto00)
+
+	for i,c in pairs(r) do
+		if c.quality==nil then else
+			hist[#hist+1] = c
+			print(col(i,3)..col(c.cellnum,3)..col(c.channel,3)..col(c.essid,18)..col(c.address,18)..col(c.quality,3)..bar(tonumber(c.quality)))
+		end
+	end
+	print(#hist)
+	
 end
 
-for l in f:lines () do
-	--print (l)
-	cellnum,address = string.match(l,"Cell (%d+)............(.*)")
-	if cellnum then
-		r[#r+1] = tt
-		tt={}
-		add("cellnum",cellnum)
-		add("address",address)
-	end
-	add("essid",string.match(l,"ESSID:.(.*)."))
-	add("channel",string.match(l,"Channel:(%d+)"))
-	add("quality",string.match(l,"Quality=(%d+).*"))
-
-	--	cellNum,n,cellAddr=string.match(l,"(%d+)(............)(.................)")
-	end
-r[#r+1] = tt
-table.remove(r,1)
-
-function bar(q)
-	local ret=""
-	for i=0,q,1 do
-		ret=ret.."|"
-	end
-	for i=0,70-q,1 do
-		ret=ret.."-"
-	end
-	return ret
+for i=0,100,1 do
+	proces()
 end
-
-function col(s,n)
-	return string.sub(s.."                              ",0,n)
-end
-
-table.sort(r,function(a,b) return a.quality>b.quality end)
-
-for i,c in pairs(r) do
-	if c.quality==nil then else
-		hist[#hist+1] = c
-		print(col(i,3)..col(c.cellnum,3)..col(c.channel,3)..col(c.essid,18)..col(c.address,18)..col(c.quality,3)..bar(tonumber(c.quality)))
-	end
-end
-
-print(#hist)
 
 --pc("31mxpa")
 --pc("32mxpa")
